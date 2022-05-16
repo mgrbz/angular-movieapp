d705aed4dc8a1b74f6fdbbec64d5bfeb68b3092c
grbz <muhammet.gurbuz@ibtech.com.tr>
2022-05-16 18:52:38 +0300

    alertify and bootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MovieCreateComponent } from './components/movie-create/movie-create.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieComponent } from './components/movies/movie/movie.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieCreateComponent,
+    MovieDetailsComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
